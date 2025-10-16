d6856852ef1f33218dba306229a331b4f4074878
Phurpa Sherpa <phurpasherpa@Phurpas-MacBook-Air.local>
2025-10-16 21:49:34 +0545

    Handle empty blogs in cards page

--- app/page.js
+++ app/page.js
@@ -5,6 +5,10 @@ import BlogFetch, { getBlogById }  from "../lib/blogFetch";
 export default async function Home() {
   const blogs = await BlogFetch()
 
+  if(!blogs){
+      return <div className='container mx-auto py-8'>Post dont have data</div>
+  }
+
   return (
     <div className="flex gap-8">
       {blogs.map((item, index)=>(
